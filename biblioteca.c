abf8a63ecba80b2cfadb4cd8ac2dcffa2ab6c459
EricSongWatanabe <146136961+EricSongWatanabe@users.noreply.github.com>
2023-11-01 18:04:35 +0000

    filtrar por categoria e estado finalizado

--- biblioteca.c
+++ biblioteca.c
@@ -192,8 +192,32 @@ int filtrarCategoria(ListadeTarefas lt){
   return 0;
 }
 
+int filtrarCategoriaPrioridade(ListadeTarefas lt){
+  int prioridade;
+  printf("\nFiltrar por Categoria e Prioridade\n");
+  printf("\nDigite a prioridade: ");
+  scanf("%d", &prioridade);
+  printf("\nDigite a categoria: ");
+  char categoria[100];
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) { } 
+  fgets(categoria, 100, stdin);
+  categoria[strcspn(categoria, "\n")] = '\0';
+  printf("\nTarefas com prioridade %d e categoria %s:\n", prioridade,
+    categoria);
+  for (int i = 0; i < lt.qtd; i++){
+    if (lt.t[i].prioridade == prioridade && strcmp(lt.t[i].categoria, categoria) == 0){
+      printf("\nLembrete %d:\n", i + 1);
+      printf("Prioridade: %d\n", lt.t[i].prioridade);
+      printf("Categoria: %s\n", lt.t[i].categoria);
+      printf("Descricao: %s\n", lt.t[i].descricao);
+      printf("Estado: %d\n", lt.t[i].estado);
+    }
+  }      
+} 
+
 void printMenu() { // imprime o menu com as opcoes
-  printf("\nMenu:\n[1] Criar Tarefa\n[2] Deletar Tarefa\n[3] Listar Tarefas\n[4] Alterar Tarefa\n[5] Filtrar por Prioridade\n[6] Filtrar por Estado\n[7] Filtrar por Categoria\n[0] Sair\nEscolha uma opcao: ");
+  printf("\nMenu:\n[1] Criar Tarefa\n[2] Deletar Tarefa\n[3] Listar Tarefas\n[4] Alterar Tarefa\n[5] Filtrar por Prioridade\n[6] Filtrar por Estado\n[7] Filtrar por Categoria\n[8] Filtrar por Categoria e Prioridade\n[0] Sair\nEscolha uma opcao: ");
 }
 
 int salvarLista(ListadeTarefas lt, char arquivo[]) {
